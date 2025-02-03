7653a65a6ecd4e7ed0b65d998a70152275917aa4
jinxcastro <work.rafaelcastro@gmail.com>
2025-02-03 17:46:01 +0800

    main

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import { useParams, Link } from 'react-router-dom';
 import DataGrid from 'react-data-grid';
 import '../styles/dashboard.css';
 import "react-data-grid/lib/styles.css";
-import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 
 const API_URL = 'http://127.0.0.1:5000/api';
 
@@ -12,33 +12,33 @@ const Dashboard: React.FC = () => {
     const { username } = useParams<{ username: string }>();
     const [users, setUsers] = useState<{ id: number; username: string }[]>([]);
 
-    // useEffect(() => {
-    //     const fetchUsers = async () => {
-    //         const response = await axios.get(`${API_URL}/dashboard/${username}`);
-    //         setUsers(response.data.users);
-    //     };
-    //     fetchUsers();
-    // }, [username]);
-
     useEffect(() => {
-      const fetchUsers = async () => {
-          const fakeUsers = Array.from({ length: 10 }, (_, index) => ({
-              id: index + 1,
-              username: faker.internet.userName(),
-          }));
+        const fetchUsers = async () => {
+            const response = await axios.get(`${API_URL}/dashboard/${username}`);
+            setUsers(response.data.users);
+        };
+        fetchUsers();
+    }, [username]);
 
-          try {
-              const response = await axios.get(`${API_URL}/dashboard/${username}`);
-              const apiUsers = response.data.users;
+  //   useEffect(() => {
+  //     const fetchUsers = async () => {
+  //         const fakeUsers = Array.from({ length: 10 }, (_, index) => ({
+  //             id: index + 1,
+  //             username: faker.internet.userName(),
+  //         }));
 
-              setUsers([...apiUsers, ...fakeUsers]);
-          } catch (err) {
-              console.error('Error fetching users from API:', err);
-              setUsers(fakeUsers);
-          }
-      };
-      fetchUsers();
-  }, [username]);
+  //         try {
+  //             const response = await axios.get(`${API_URL}/dashboard/${username}`);
+  //             const apiUsers = response.data.users;
+
+  //             setUsers([...apiUsers, ...fakeUsers]);
+  //         } catch (err) {
+  //             console.error('Error fetching users from API:', err);
+  //             setUsers(fakeUsers);
+  //         }
+  //     };
+  //     fetchUsers();
+  // }, [username]);
 
     const handleDelete = async (userId: number) => {
         try {
